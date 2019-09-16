c089123d2cce4a04499f45194cd9d15b3ce62780
zhuht11 <54836457+zhuht11@users.noreply.github.com>
2019-09-16 19:36:20 +0800

    Create WelcomeWithThreeMessages.java

---
+++ WelcomeWithThreeMessages.java
@@ -0,0 +1,11 @@
+package hello;
+import java.util.*;
+public class helloworld 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Programming is fun!");
+		System.out.println("Fundamentals First");
+		System.out.println("Problem Driven");
+	}
+}
