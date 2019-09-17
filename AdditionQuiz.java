7062bd7f1ed166ca79f9a91407401d251db4b8cd
zhuht11 <54836457+zhuht11@users.noreply.github.com>
2019-09-17 15:15:27 +0800

    Create AdditionQuiz.java

---
+++ AdditionQuiz.java
@@ -0,0 +1,18 @@
+package hellojavac;
+
+import java.util.*;
+
+public class hahaha {
+	
+	public static void main(String[] args) 
+	{
+		int number1 = (int)(System.currentTimeMillis() % 10);
+		int number2 = (int)(System.currentTimeMillis() / 7 % 10);
+		Scanner input = new Scanner(System.in);
+		System.out.print("What is " + number1 + " + " + number2 + "? ");
+		int answer = input.nextInt();
+		System.out.println(number1 + " + " + number2 + " = " + answer + " is " + (number1 + number2 == answer));
+		
+	}
+}
+
